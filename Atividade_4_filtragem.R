473b9df9ab0d0c3620d9bf58cf6e27b973113a05
fergand <ffandra@gmail.com>
2023-07-01 00:25:42 -0300

    criei uma repositório para salvar as atividades

---
+++ Atividade_4_filtragem.R
@@ -0,0 +1,18 @@
+# Atividade 4 - Filtragem dos dados
+# filtrar os dados do datset do pokemon com os pokemons que possuem bee ou saur
+# no nome.
+
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+dados <- read.csv("Atividades/Dados")
+head(dados) #? vê as primeiras linhas de dados
+
+dados %>% filter(grepl("bee", name) | grepl("saur", name))
+
+df_bee_e_saur_nome <- dados %>% filter(grepl("bee", name) | grepl("saur", name))
+df_bee_e_saur_nome
+
+# Podemos observar que há 7 opçoes para o filtro desejado.
+
